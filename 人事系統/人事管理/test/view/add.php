864bc5c8420af86d4355c6776061b3af1ae2f305
jeffchang <jeffchang@ictrc.nknu.edu.tw>
2019-12-10 08:49:37 +0800

    icon fixed

--- 人事系統/人事管理/test/view/add.php
+++ 人事系統/人事管理/test/view/add.php
@@ -274,7 +274,7 @@
         dataType:'json',
         success:function(response){
           $(response).each(function(eachid){
-            $('[name=showDepartment]').append('<li class="list-group-item">'+this.department_id+'\t'+this.department_name+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="department" data-id="'+this.department_name+'" ><i class="glyphicon glyphicon-remove-circle"></i></button>');
+            $('[name=showDepartment]').append('<li class="list-group-item">'+this.department_id+'\t'+this.department_name+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="department" data-id="'+this.department_name+'" ><i class="fas fa-trash"></i></button>');
             
           });
         } 
@@ -286,7 +286,7 @@
         dataType:'json',
         success:function(response){
           $(response).each(function(eachid){
-            $('[name=showPosition]').append('<li class="list-group-item">'+this.position_id+'\t'+this.position_name+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="position" data-id="'+this.position_name+'" ><i class="glyphicon glyphicon-remove-circle"></i></button>');
+            $('[name=showPosition]').append('<li class="list-group-item">'+this.position_id+'\t'+this.position_name+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="position" data-id="'+this.position_name+'" ><i class="fas fa-trash"></i></button>');
             
           });
         } 
@@ -298,7 +298,7 @@
         dataType:'json',
         success:function(response){
           $(response).each(function(eachid){
-            $('[name=showGender]').append('<li class="list-group-item">'+this.type+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="gender" data-id="'+this.type+'" ><i class="glyphicon glyphicon-remove-circle"></i></button>');
+            $('[name=showGender]').append('<li class="list-group-item">'+this.type+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="gender" data-id="'+this.type+'" ><i class="fas fa-trash"></i></button>');
             
           });
         } 
@@ -310,7 +310,7 @@
         dataType:'json',
         success:function(response){
           $(response).each(function(eachid){
-            $('[name=showMarriage]').append('<li class="list-group-item">'+this.type+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="marriage" data-id="'+this.type+'" ><i class="glyphicon glyphicon-remove-circle"></i></button>');
+            $('[name=showMarriage]').append('<li class="list-group-item">'+this.type+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="marriage" data-id="'+this.type+'" ><i class="fas fa-trash"></i></button>');
             
           });
         } 
@@ -322,7 +322,7 @@
         dataType:'json',
         success:function(response){
           $(response).each(function(eachid){
-            $('[name=showInsuredCompany]').append('<li class="list-group-item">'+this.companyName+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="insuredcompany" data-id="'+this.companyName+'" ><i class="glyphicon glyphicon-remove-circle"></i></button>');
+            $('[name=showInsuredCompany]').append('<li class="list-group-item">'+this.companyName+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="insuredcompany" data-id="'+this.companyName+'" ><i class="fas fa-trash"></i></button>');
             
           });
         } 
@@ -334,7 +334,7 @@
         dataType:'json',
         success:function(response){
           $(response).each(function(eachid){
-            $('[name=showWorkStatus]').append('<li class="list-group-item">'+this.status+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="workStatus" data-id="'+this.status+'" ><i class="glyphicon glyphicon-remove-circle"></i></button>');
+            $('[name=showWorkStatus]').append('<li class="list-group-item">'+this.status+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="workStatus" data-id="'+this.status+'" ><i class="fas fa-trash"></i></button>');
             
           });
         } 
@@ -345,7 +345,7 @@
         dataType:'json',
         success:function(response){
           $(response).each(function(eachid){
-            $('[name=showStaffType]').append('<li class="list-group-item">'+this.type+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="staffType" data-id="'+this.type+'" ><i class="glyphicon glyphicon-remove-circle"></i></button>');
+            $('[name=showStaffType]').append('<li class="list-group-item">'+this.type+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="staffType" data-id="'+this.type+'" ><i class="fas fa-trash"></i></button>');
             
           });
         } 
@@ -357,7 +357,7 @@
         dataType:'json',
         success:function(response){
           $(response).each(function(eachid){
-            $('[name=showEducationCondition]').append('<li class="list-group-item">'+this.type+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="educationCondition" data-id="'+this.type+'" ><i class="glyphicon glyphicon-remove-circle"></i></button>');
+            $('[name=showEducationCondition]').append('<li class="list-group-item">'+this.type+'<button type="button" class="btn  btn-xs pull-right"  data-toggle="modal" data-target="#deleteModel" data-type="educationCondition" data-id="'+this.type+'" ><i class="fas fa-trash"></i></button>');
             
           });
         } 
